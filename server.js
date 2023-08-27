71f36cf848c9eb52f67a58c8817a9a5456c8d3d6
kumarram96 <128773696+kumarram96@users.noreply.github.com>
2023-08-27 17:16:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello green World! - welcome')
+  res.send('Hello green World! - welcome to tirunelveli')
 })
 
 app.listen(port, () => {
